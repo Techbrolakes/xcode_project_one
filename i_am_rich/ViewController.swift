67ec5a9be6fe558660b51d3fca82d3417e34e6a7
Techbrolakes <lekandar11@gmail.com>
2023-09-27 17:15:52 +0100

    Initial Commit

---
+++ i_am_rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_rich
+//
+//  Created by Olamilekan Daramola on 27/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
